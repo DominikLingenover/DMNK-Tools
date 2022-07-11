e7e3e5c858f1205a92622e77ed51c24183442ce7
DominikLingenover <lingenoverdominik@gmail.com>
2022-07-11 22:11:01 +0200

    New Proxy from Node Tool

--- python3.7libs/dmnkshelfutils.py
+++ python3.7libs/dmnkshelfutils.py
@@ -13,3 +13,21 @@ def instance_from_node():
             instance_node.moveToGoodPosition()
 
             node.setDisplayFlag(False)
+
+def proxy_from_node():
+    sel_nodes = hou.selectedNodes()
+
+    for node in sel_nodes:
+        if node.type().name() == "geo":
+            display_node = node.displayNode()
+            proxy_output_node = display_node.createOutputNode("Redshift_Proxy_Output")
+            proxy_output_node.parm("execute").pressButton()
+            
+            proxy_loader_node = node.parent().createNode("geo")
+            proxy_loader_node.createNode("redshift_proxySOP")
+            proxy_loader_node.setParms({
+                "RS_objprop_proxy_enable": 1,
+                "RS_objprop_proxy_file": proxy_output_node.parm("RS_archive_file").eval()
+            })
+            
+            node.setDisplayFlag(False)
